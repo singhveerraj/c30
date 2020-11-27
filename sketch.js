a8c1c5d934a431e4edba3be05de028cbf7272963
singhveerraj <singhveerraj.rv@gmail.com>
2020-11-27 18:52:58 +0530

    c30

--- sketch.js
+++ sketch.js
@@ -75,3 +75,13 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+function keyPressed(){
+    if (keyCode===32){
+        slingshot.attach(bird.body);
+    }
+}
+
+
+
+
